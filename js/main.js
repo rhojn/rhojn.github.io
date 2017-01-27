3b18c7c3d6a8300a8e99c6e65ce1a6c444b552d6
rhojn <rhojeno8@gmail.com>
2017-01-27 10:35:48 +0800

    finish main half-1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$( document ).ready(function() {
+      $("#arrow-btn").click(function() {
+            $("#front").slideUp("500", "swing");
+             $("#main").removeClass( "hidden");
+        }
+    );
+});
