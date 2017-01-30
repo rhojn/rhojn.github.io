0d2ffd26bed29fd61f2eafd59482899cb5b2ccdb
rhojn <rhojeno8@gmail.com>
2017-01-30 11:44:33 +0800

    v2

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-$( document ).ready(function() {
-      $("#arrow-btn").click(function() {
-            $("#front").slideUp("500", "swing");
-             $("#main").removeClass( "hidden");
-        }
-    );
-});
